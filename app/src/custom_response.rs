0907c12b2f64e25e01874e22108b9cf09b614e37
liufankai <liufankai137@outlook.com>
2022-12-07 22:56:11 +0800

    add error msg to response context

--- app/src/custom_response.rs
+++ app/src/custom_response.rs
@@ -128,33 +128,6 @@ where
 }
 
 #[derive(Debug)]
-pub struct ResJsonString(pub String);
-pub trait ResultExt<T> {
-    /// If `self` contains a SQLx database constraint error with the given name,
-    /// transform the error.
-    ///
-    /// Otherwise, the result is passed through unchanged.
-    fn on_constraint(
-        self,
-        name: &str,
-        f: impl FnOnce(Box<dyn DatabaseError>) -> Error,
-    ) -> Result<T, Error>;
-}
+pub struct ResJsonString(pub String); 
 
-impl<T, E> ResultExt<T> for Result<T, E>
-where
-    E: Into<Error>,
-{
-    fn on_constraint(
-        self,
-        name: &str,
-        map_err: impl FnOnce(Box<dyn DatabaseError>) -> Error,
-    ) -> Result<T, Error> {
-        self.map_err(|e| match e.into() {
-            Error::Sqlx(sqlx::Error::Database(dbe)) if dbe.constraint() == Some(name) => {
-                map_err(dbe)
-            }
-            e => e,
-        })
-    }
-}
+
