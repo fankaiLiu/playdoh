71adb9e14355fbe76f7df58ec8c8283be8a12d55
liufankai <liufankai137@outlook.com>
2023-01-15 14:37:08 +0800

    :bug: Fix parsing cookie error

--- app/src/utils/jwt.rs
+++ app/src/utils/jwt.rs
@@ -20,7 +20,7 @@ pub static KEYS: Lazy<Keys> = Lazy::new(|| {
     Keys::new(secret.as_bytes())
 });
 use configs::CFG;
-use tower_cookies::{Cookies, Cookie};
+use tower_cookies::{Cookie, Cookies};
 
 pub struct Keys {
     pub encoding: EncodingKey,
@@ -57,11 +57,8 @@ where
 {
     type Rejection = AuthError;
     /// 将用户信息注入request
-    async fn from_request_parts(
-        req: &mut Parts,
-        _state: &S,
-    ) -> Result<Self, Self::Rejection> {
-        let (_, token_v) = get_cookie_token(req).await?;
+    async fn from_request_parts(req: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let token_v = get_cookie_token(req).await?;
         //获取 cookie
         let token_data = match decode::<Claims>(&token_v, &KEYS.decoding, &Validation::default()) {
             Ok(token) => {
@@ -100,27 +97,26 @@ where
     }
 }
 
-pub async fn get_cookie_token(parts: &mut Parts) -> Result<(String, String), AuthError>
+pub async fn get_cookie_token(parts: &mut Parts) -> Result<String, AuthError>
 where
 {
     let cookie = parts
-    .extract::<Cookies>() .await
-    .map_err(|_| AuthError::InvalidToken)?;
-    // let TypedHeader(Authorization(bearer)) = parts
-    //     .extract::<TypedHeader<Authorization<Bearer>>>()
-    //     .await
-    //     .map_err(|_| AuthError::InvalidToken)?;
-    // //Decode the user data
-    // let bearer_data = bearer.token();
-    let token_data= &cookie.get("token").ok_or(AuthError::InvalidToken)?.to_string();
-    let cut = token_data.len() - scru128::new_string().len();
-    Ok((
-        token_data[cut..].to_string(),
-        token_data[0..cut].to_string(),
-    ))
+        .extract::<Cookies>()
+        .await
+        .map_err(|_| AuthError::InvalidToken)?;
+    let token_data = &cookie
+        .get("token")
+        .ok_or(AuthError::InvalidToken)?
+        .to_string();
+
+    Ok(token_data[6..].to_string())
 }
 
-pub async fn authorize(cookies:Cookies,payload: AuthPayload, token_id: String) -> Result<AuthBody, AuthError> {
+pub async fn authorize(
+    cookies: Cookies,
+    payload: AuthPayload,
+    token_id: String,
+) -> Result<AuthBody, AuthError> {
     if payload.user_id.is_empty() || payload.name.is_empty() {
         return Err(AuthError::MissingCredentials);
     }
@@ -135,9 +131,10 @@ pub async fn authorize(cookies:Cookies,payload: AuthPayload, token_id: String) -
     // Create the authorization token
     let token = encode(&Header::default(), &claims, &KEYS.encoding)
         .map_err(|_| AuthError::WrongCredentials)?;
+    dbg!(&token);
     // Send the authorized token
-        // Build the cookie
-    let mut cookie=Cookie::new("token", token.clone());
+    // Build the cookie
+    let mut cookie = Cookie::new("token", token.clone());
     cookie.set_http_only(true);
     cookies.add(cookie);
     Ok(AuthBody::new(token, claims.exp, CFG.jwt.jwt_exp, token_id))
